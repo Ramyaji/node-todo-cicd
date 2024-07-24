8b4b7ca163712728ec4476491bb7850e08bef042
Ramya_Yata <89580700+Ramyaji@users.noreply.github.com>
2024-07-24 17:35:30 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,39 @@
+</head>
+
+</style>
+
+<body>
+
+<h1> Automating Deployment</h1>
+
+<h1>Ramya</h1>
+
+<h2>ramya@gmail.com</h2>
+
+<h2>This is my jenkins server project automatically deployment </h2>
+
+<h2>successfully, build and deploy CICD project!!!</h2>
+
+<h3>Awesome automation tools!! just Love it</h3>
+
+<u>
+
+todolist.forEach(function(todo, index) {
+
+<li>
+
+<a href="/todo/delete/index >">x</a>
+
+<a href="/todo/< index>"></a>
+
+todo
+
+</li>
+
+4});
+
+</ul>
+
+<form action="/todo/add/" method="post">
+
+<p>
